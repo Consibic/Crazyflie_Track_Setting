f01b0be4c7e710b3447167317532fc463a46ca36
Constantine Cheng <boyangcheng123@gmail.com>
2025-06-10 17:18:57 -0400

    Add files via upload

---
+++ build/extras/vicon_bridge/CMakeFiles/tf_distort.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/src/tf_distort.cpp" "/home/fsc1/Github/vicon_ws/build/extras/vicon_bridge/CMakeFiles/tf_distort.dir/src/tf_distort.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_ATOMIC_DYN_LINK"
+  "BOOST_THREAD_DYN_LINK"
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"vicon_bridge\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/fsc1/Github/vicon_ws/devel/include"
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/src"
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/include"
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/vicon_sdk/Vicon_SDK_1.3_Linux/64-bit"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
