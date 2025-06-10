61d439d6875c22b0c3803050b4ed7d35312c7c43
Constantine Cheng <boyangcheng123@gmail.com>
2025-06-10 17:09:40 -0400

    Add files via upload

---
+++ build/extras/vicon_bridge/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/fsc1/Github/vicon_ws/devel/include".split(';') if "/home/fsc1/Github/vicon_ws/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "dynamic_reconfigure;geometry_msgs;message_runtime;roscpp;nodelet".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lvicon_bridge_lib".split(';') if "-lvicon_bridge_lib" != "" else []
+PROJECT_NAME = "vicon_bridge"
+PROJECT_SPACE_DIR = "/home/fsc1/Github/vicon_ws/devel"
+PROJECT_VERSION = "1.0.1"
---
+++ build/extras/vicon_bridge/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "dynamic_reconfigure;geometry_msgs;message_runtime;roscpp;nodelet".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lvicon_bridge_lib".split(';') if "-lvicon_bridge_lib" != "" else []
+PROJECT_NAME = "vicon_bridge"
+PROJECT_SPACE_DIR = "/home/fsc1/Github/vicon_ws/install"
+PROJECT_VERSION = "1.0.1"
