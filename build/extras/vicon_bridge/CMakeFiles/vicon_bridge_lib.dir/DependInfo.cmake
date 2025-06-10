cf3cd4af0b5bc868197b1302fbd874739b1152c0
Constantine Cheng <boyangcheng123@gmail.com>
2025-06-10 17:20:38 -0400

    Add files via upload

---
+++ build/extras/vicon_bridge/CMakeFiles/vicon_bridge_lib.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/src/vicon_bridge.cpp" "/home/fsc1/Github/vicon_ws/build/extras/vicon_bridge/CMakeFiles/vicon_bridge_lib.dir/src/vicon_bridge.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"vicon_bridge\""
+  "vicon_bridge_lib_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/fsc1/Github/vicon_ws/devel/include"
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/src"
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/include"
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/home/fsc1/Github/vicon_ws/src/extras/vicon_bridge/vicon_sdk/Vicon_SDK_1.3_Linux/64-bit"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/fsc1/Github/vicon_ws/build/extras/vicon_bridge/CMakeFiles/msvc_bridge.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/extras/vicon_bridge/CMakeFiles/vicon_bridge_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/fsc1/Github/vicon_ws/devel/lib/libvicon_bridge_lib.pdb"
+  "/home/fsc1/Github/vicon_ws/devel/lib/libvicon_bridge_lib.so"
+  "CMakeFiles/vicon_bridge_lib.dir/src/vicon_bridge.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vicon_bridge_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
